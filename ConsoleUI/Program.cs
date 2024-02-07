1fa02219d531b46d295893d6125aa0307e46f139
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 13:40:15 -0500

    Create Code
    Built a Console Application that asks for the user's name. If their name is Tim, say "Hello Professor". Otherwise, say "Hi __". Do this until they type "exit".

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			string name;
+
+			do
+			{
+				Console.Write("What is your name: ");
+				name = Console.ReadLine();
+				if ( name.ToLower() == "tim" )
+				{
+					Console.WriteLine("Hello Professor");
+				}
+				else if ( name.ToLower() != "exit" )
+				{
+					Console.WriteLine($"Hi {name}");
+				}
+			} while ( name.ToLower() != "exit" );
 		}
 	}
 }
